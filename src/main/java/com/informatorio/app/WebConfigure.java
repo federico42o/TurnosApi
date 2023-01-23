f546e62083e2a9f0f35b2a4a399d3ff307f5b380
federicos1 <fedes7777@gmail>
2023-01-22 22:57:26 -0300

    fix: minor fix

--- src/main/java/com/informatorio/app/WebConfigure.java
+++ src/main/java/com/informatorio/app/WebConfigure.java
@@ -24,7 +24,7 @@ public class WebConfigure extends WebMvcConfigurationSupport {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true);
     }
